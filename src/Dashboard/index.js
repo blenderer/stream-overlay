bc78242eba4d5d56fb06b1cff07e99d16849072e
Eric Harrison <eric.h@rrison.io>
2018-05-06 20:57:02 -0400

    scaffold scoreboard files

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -1,26 +1,12 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
+import TextField from 'material-ui/TextField';
 
 class Dashboard extends Component {
-  styles = {
-    container: {
-      display: 'flex',
-      flexDirection: 'row',
-      alignItems: 'center'
-    }
-  }
-
-  displayReplicant = window.nodecg.Replicant('test')
-
-  onChange = (e) => {
-    this.displayReplicant.value = e.target.value;
-  }
 
   render() {
     return (
-      <div style={this.styles.container}>
-        <img style={{width: 100}} src={`build${logo}`} alt=""/>
-        <input onChange={this.onChange} type="text"/>
+      <div style={{padding: 10}}>
+        <TextField placeholder='hello world' />
       </div>
     );
   }
