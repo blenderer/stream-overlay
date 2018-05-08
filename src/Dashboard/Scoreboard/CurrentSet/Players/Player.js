dbc3cae093df4bc6bf6ae5efb5ae26b21eb429f1
Eric Harrison <eric.h@rrison.io>
2018-05-07 23:56:51 -0400

    add autocomplete implementation

--- src/Dashboard/Scoreboard/CurrentSet/Players/Player.js
+++ src/Dashboard/Scoreboard/CurrentSet/Players/Player.js
@@ -1,13 +1,76 @@
 import React, { Component } from 'react';
 import Grid from 'material-ui/Grid';
 import TextField from 'material-ui/TextField';
+import Autosuggest from 'react-autosuggest';
+
+import characters from '../../../../data/smash4/characters';
 
 import { withStyles } from 'material-ui/styles';
 
+// Teach Autosuggest how to calculate suggestions for any given input value.
+const getSuggestions = value => {
+  const inputValue = value.trim().toLowerCase();
+  const inputLength = inputValue.length;
+
+  return inputLength === 0 ? [] : characters.filter(character =>
+    character.toLowerCase().slice(0, inputLength) === inputValue
+  );
+};
+
+// When suggestion is clicked, Autosuggest needs to populate the input
+// based on the clicked suggestion. Teach Autosuggest how to calculate the
+// input value for every given suggestion.
+const getSuggestionValue = suggestion => suggestion;
+
+// Use your imagination to render suggestions.
+const renderSuggestion = suggestion => (
+  <div>
+    {suggestion}
+  </div>
+);
+
+const renderInputComponent = inputProps => (
+  <TextField {...inputProps} />
+);
+
 class Player extends Component {
+
+  state = {
+    value: '',
+    suggestions: []
+  };
+
+  onChange = (event, { newValue }) => {
+    this.setState({
+      value: newValue
+    });
+  };
+
+  // Autosuggest will call this function every time you need to update suggestions.
+  // You already implemented this logic above, so just use it.
+  onSuggestionsFetchRequested = ({ value }) => {
+    this.setState({
+      suggestions: getSuggestions(value)
+    });
+  };
+
+  // Autosuggest will call this function every time you need to clear suggestions.
+  onSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: []
+    });
+  };
 
   render() {
     const { classes, number } = this.props;
+    const { value, suggestions } = this.state;
+
+    const inputProps = {
+      placeholder: 'Character',
+      value,
+      onChange: this.onChange
+    };
+
     return (
       <Grid container direction='column' spacing={16}>
         <Grid item>
@@ -28,7 +91,16 @@ class Player extends Component {
         </Grid>
         <Grid item container direction='row' spacing={16}>
           <Grid item>
-            <TextField placeholder='Character' />
+            <Autosuggest
+              suggestions={suggestions}
+              onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+              onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+              getSuggestionValue={getSuggestionValue}
+              renderSuggestion={renderSuggestion}
+              inputProps={inputProps}
+              renderInputComponent={renderInputComponent}
+            />
+            {/* <TextField placeholder='Character' /> */}
           </Grid>
         </Grid>
       </Grid>
--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
 import Scoreboard from './Scoreboard';
 
-import characters from '../data/smash4/characters';
-
 class Dashboard extends Component {
 
   render() {
     return (
       <div style={{padding: 10, overflow: 'hidden'}}>
-        {characters}
         <Scoreboard />
       </div>
     );
