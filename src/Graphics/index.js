02486fea7de70b247d768e4d12603d02a0bf6192
Eric Harrison <eric.h@rrison.io>
2018-05-17 21:34:12 -0400

    add smash n splash graphics and add name to overlay

--- src/Graphics/index.js
+++ src/Graphics/index.js
@@ -1,39 +1,84 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
+import singlesBase from './frostbite-graphics/Overlays/SNS4-Gameplay-Singles-Base.png';
+import singlesLeft from './frostbite-graphics/Overlays/SNS4_Scoreboard-Singles-Left.png';
+import singlesRight from './frostbite-graphics/Overlays/SNS4_Scoreboard-Singles-Right.png';
 
 import NodeCGReplicant from '../Dashboard/NodeCGReplicant';
 
 class Graphics extends Component {
+	state = {
+		scoreboard: null
+	};
 
-  state = {
-    scoreboard: null
-  }
+	styles = {
+		graphic: {
+			position: 'absolute',
+			top: 0,
+			left: 0,
+			right: 0,
+			bottom: 0
+		},
+		name: {
+			top: 6,
+			width: 411,
+			textAlign: 'center',
+      color: 'white',
+      fontSize: 39,
+      position: 'absolute'
+		}
+	};
 
-  renderGraphics () {
-    const { scoreboard } = this.state;
+	renderGraphics() {
+		const { scoreboard } = this.state;
+		const styles = this.styles;
 
-    return (
-      <div>
-        <img style={{width: 50}} src={`build${logo}`} alt=""/>
-        <h1>{scoreboard.format}</h1>
-      </div>
-    );
-  }
+		return (
+			<div style={{ position: 'relative' }}>
+				<img
+					style={styles.graphic}
+					src={`build${singlesBase}`}
+					alt=""
+				/>
+				<img
+          style={styles.graphic}
+					src={`build${singlesLeft}`}
+					alt=""
+				/>
+        <img
+					style={styles.graphic}
+					src={`build${singlesRight}`}
+					alt=""
+				/>
+				<div
+          style={{ ...styles.name, left: 2 }}
+				>
+					{scoreboard.players[0].name}
+				</div>
+        <div
+          style={{ ...styles.name, right: 2 }}
+				>
+					{scoreboard.players[1].name}
+				</div>
+			</div>
+		);
+	}
 
-  render() {
-    const { scoreboard } = this.state;
+	render() {
+		const { scoreboard } = this.state;
 
-    return (
-      <React.Fragment>
-        <NodeCGReplicant
-          replicantName='scoreboard'
-          value={this.state.scoreboard}
-          onNewValue={(newValue) => {this.setState({scoreboard: newValue})}}
-        />
-        {scoreboard ? this.renderGraphics() : null}
-      </React.Fragment>
-    );
-  }
+		return (
+			<React.Fragment>
+				<NodeCGReplicant
+					replicantName="scoreboard"
+					value={this.state.scoreboard}
+					onNewValue={newValue => {
+						this.setState({ scoreboard: newValue });
+					}}
+				/>
+				{scoreboard ? this.renderGraphics() : null}
+			</React.Fragment>
+		);
+	}
 }
 
 export default Graphics;
