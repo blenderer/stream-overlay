3c0ededa656518b2fa87392fb795ccafdd30d893
Eric Harrison <eric.h@rrison.io>
2018-05-29 22:37:20 -0400

    mid set reports the actual score

--- src/Graphics/smashnsplash/components/MidSetReport.js
+++ src/Graphics/smashnsplash/components/MidSetReport.js
@@ -13,15 +13,25 @@ class MidSetReport extends PureComponent {
 	render() {
 		const { scoreboard, classes, enabled } = this.props;
 
+    console.log(scoreboard.set.player1);
+
+    const player1 = [
+      <GraphicImage key={0} src={`build${graphics.ggBo31L}`} />,
+      <GraphicImage key={1} src={`build${graphics.ggBo32L}`} />
+    ];
+
+    const player2 = [
+      <GraphicImage key={0} src={`build${graphics.ggBo31R}`} />,
+      <GraphicImage key={1} src={`build${graphics.ggBo32R}`} />
+    ];
 
 		return (
 			<Graphic enabled={enabled}>
         <GraphicImage src={`build${graphics.ggCamBase}`} />
         <GraphicImage src={`build${graphics.ggCamBo3}`} />
-        <GraphicImage src={`build${graphics.ggBo31L}`} />
-        <GraphicImage src={`build${graphics.ggBo31R}`} />
-        <GraphicImage src={`build${graphics.ggBo32L}`} />
-				<GraphicImage src={`build${graphics.ggBo32R}`} />
+        {player1.slice(0, scoreboard.set.player1)}
+        {player2.slice(0, scoreboard.set.player2)}
+
 			</Graphic>
 		);
 	}
