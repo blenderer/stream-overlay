fad77eb4429c762964af98b315e5dc221cf564c7
Eric Harrison <eric.h@rrison.io>
2018-11-13 20:37:30 -0500

    Add swap sides functionality to commentators

--- src/Dashboard/Scoreboard/Commentary/index.js
+++ src/Dashboard/Scoreboard/Commentary/index.js
@@ -1,50 +1,70 @@
-import React, { Component } from 'react';
-import Grid from '@material-ui/core/Grid';
+import React, { Component } from "react";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
 
-import Commentator from './Commentator';
+import Commentator from "./Commentator";
 
-import { withStyles } from '@material-ui/core/styles';
+import { withStyles } from "@material-ui/core/styles";
 
 class Commentary extends Component {
+  handleChange = (index, value) => {
+    const { scoreboard } = this.props;
+    const newCommentators = [...scoreboard.commentators];
 
-	handleChange = (index, value) => {
-		const { scoreboard } = this.props;
-		const newCommentators = [...scoreboard.commentators];
+    newCommentators[index] = value;
 
-		newCommentators[index] = value;
+    this.props.onChange({
+      ...scoreboard,
+      commentators: newCommentators
+    });
+  };
 
-		this.props.onChange({
-			...scoreboard,
-			commentators: newCommentators
-		});
-	};
+  swap = () => {
+    const { scoreboard } = this.props;
+    const newCommentators = [...scoreboard.commentators];
 
-	render () {
-		const { scoreboard } = this.props;
+    const temp = newCommentators[0];
+    newCommentators[0] = newCommentators[1];
+    newCommentators[1] = temp;
 
-		return (
-			<Grid container direction="row" spacing={16}>
+    this.props.onChange({
+      ...scoreboard,
+      commentators: newCommentators
+    });
+  };
 
+  render() {
+    const { scoreboard } = this.props;
+
+    return (
+      <React.Fragment>
+        <div>
+          <Button onClick={this.swap} variant="contained">
+            Swap Sides
+          </Button>
+        </div>
+        <Grid container direction="row" spacing={16}>
           {scoreboard.commentators.map((commentator, index) => (
             <Grid key={index} item>
               <Commentator
-    						number={index}
-    						model={commentator}
-                onChange={(value) => {this.handleChange(index, value)}}
-    					/>
+                number={index}
+                model={commentator}
+                onChange={value => {
+                  this.handleChange(index, value);
+                }}
+              />
             </Grid>
-
           ))}
-			</Grid>
-		);
-	}
-
+        </Grid>
+      </React.Fragment>
+    );
+  }
 }
 
 const styles = theme => ({
-	sponsor: {
-		width: 60
-	}
+  sponsor: {
+    width: 60
+  }
 });
 
 export default withStyles(styles)(Commentary);
